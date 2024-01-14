10cb2bc0a5e6c589e0bdadda7384d4734f71c405
“Edwin <oriokie@icloud.com>
2024-01-14 23:12:13 +0300

    removed help methods

--- console.py
+++ console.py
@@ -10,27 +10,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, args):
         """Quit command to exit the program."""
-        print("Quitting.")
-        return True  # Signal to cmdloop to quit
+        return True
 
     def do_EOF(self, args):
         """Exit on end-of-file character (Ctrl-D)."""
-        print("Quitting.")
         return True
-
-    def help_quit(self):
-        """
-        Help Method for quitting
-
-        """
-        print("Quit command to exit the program.")
-
-    def help_EOF(self):
-        """
-        Help Method for end of file
-
-        """
-        print("Exit on end-of-file character (Ctrl-D).")
 
     def emptyline(self):
         """Do nothing on empty input."""
