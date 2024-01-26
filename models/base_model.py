9f3fc6ed1dd56efef65ba79fe6661cb79f8a9022
V11an <allankertich@gmail.com>
2024-01-26 18:02:42 +0000

    d

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,26 @@
-#!/usr/bin/env python3
-"""
-This is the base model
+#!/usr/bin/python3
 
-"""
-from datetime import datetime
 import uuid
-import models
+from datetime import datetime
 
-
-class BaseModel():
-    """
-    THis is the base class
-
-    """
+class BaseModel:
     def __init__(self, *args, **kwargs):
-        """
-        Initializing the variables
-
-        id (int): public instance variables
-        created_at: datetime: time when the instance was created
-        updated_at: datetime: time when the instance was created
-
-        """
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key in ('created_at', 'updated_at'):
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        String representation
-        should print: [<class name>] (<self.id>) <self.__dict__>
-
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id,
-                                     self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-        Method for updating the updated_at public instance attribute
-
-        """
-        self.updated_at = datetime.now()
-        models.storage.save()
+    self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns the dictionary format of the instance
-        """
-        air_dict = self.__dict__.copy()
-        air_dict['__class__'] = self.__class__.__name__
-        air_dict['created_at'] = self.created_at.isoformat()
-        air_dict['updated_at'] = self.updated_at.isoformat()
-        return air_dict
+        return {
+                "__class__": self.__class__.__name__,
+                **{key: value.isoformat() if isinstance(value, datetime) else value for key, value in self.__dict__.items()}
+                }
